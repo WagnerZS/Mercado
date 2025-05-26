24bdaf76411e158d6db8b242348a0c76d2287b37
Wagner Zanuzzi Schmitt <1137242@atitus.edu.br>
2025-05-26 01:08:07 +0000

    Alterações para deploy do front end no github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// Altere 'seu-usuario' e 'seu-repo' para o seu usuário e repositório
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  base: '/Mercado/', // coloque o nome do seu repositório aqui
+  plugins: [react(), tailwindcss()],
 })
